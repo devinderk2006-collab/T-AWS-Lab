1a7cf182c33970c35a21ca4a98f06324b2760771
Devinder Kaur <devinder.k2006@gmail.com>
2026-09-18 11:59:40 +0530

    Added main.tf
    Initialize Terraform configuration for AWS provider and S3 bucket.

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket_prefix = "iac-lab-devinder-"
+}
